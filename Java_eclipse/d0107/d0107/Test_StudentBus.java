d9522edabdd44b1cbb663cbe6d8630719f412173
Cherta <Cherta4242@gmail.com>
2026-01-07 18:08:50 +0900

    Add files via upload

---
+++ Java_eclipse/d0107/d0107/Test_StudentBus.java
@@ -0,0 +1,21 @@
+package d0107;
+
+public class Test_StudentBus {
+
+	public static void main(String[] args) {
+		StudentBus stdb1 = new StudentBus("장진우", 2, 2000);
+		StudentBus stdb2 = new StudentBus("김건엽", 2, 5000);
+		BusStudent bus1 = new BusStudent("47");		
+
+		bus1.busInfo();
+		
+		stdb2.getOnBus(bus1);
+		System.out.println(stdb1.stuInfo());
+		System.out.println(stdb2.stuInfo());
+		
+		System.out.println( bus1.busInfo());
+		
+		
+	}
+
+}
---
+++ Java_eclipse/d0107/d0107/Unit.java
@@ -0,0 +1,68 @@
+package d0107;
+
+public class Unit {
+	
+	// 체력은 5 이름은 red 거나 blue 거나.
+	// 블루의 에너지는 3. 예? 집중좀 하자.
+	// 공격받은 아이는 2만큼 줄어든다. 
+	// 정보은닉 적용
+	
+	private String unitName;	// 생성되는 유닛의 이름
+	private int energy;	// 생성되늰 unit의 체력 = 5
+	private int damage;
+	
+	
+	// ------------------------------------------------------	
+	// 유닛의 스텟 설정하기.
+	/*
+	Unit(String unitName, int Damage, int energy){
+		if(unitName != "blue") {
+			System.out.println("생성되는 유닛의 이름이 RED로 설정되었습니다.");
+			this.unitName = "RED";
+		}else {
+			System.out.println("생성되는 유닛의 이름이 BLUE로 설정되었습니다.");
+			this.unitName = "BLUE";
+		}
+		if(Damage >= 5) {
+			System.out.println("저희 유닛의 최대 공격력인 4로 설정되었습니다.");
+			this.damage = 4;
+		}else {
+			System.out.println("공격력이 설정되었습니다.");
+			this.damage = Damage;
+		}
+		if(energy > 15) {
+			System.out.println("저희 유닛의 최대 체력인 15로 설정되었습니다.");
+			this.energy = 15;
+		}else {
+			System.out.println("유닛의 체력이 설정되었습니다.");
+			this.energy = energy;
+		}
+		System.out.println("유닛 설정이 완료되었습니다.");
+	}
+	*/
+	
+
+	/*
+	void unitInfo() {
+		return "유닛 이름: " + unitName + "\n유닛 체력: " + energy + "데미지" + damage;
+	}
+	*/
+	// ------------------------------------------------------
+	
+	// Unit Name 설정
+	void setUnitname(String unitName) {
+		this.unitName = unitName;
+	}
+	String getUnitName() {
+		return unitName;
+	}
+	
+	// Unit Damage 설정
+	void setUnitDmg(int damage) {
+		this.damage = damage;
+	}
+	int getUnitDmg() {
+		return damage;
+	}
+	// 집가서 해보자.
+}
